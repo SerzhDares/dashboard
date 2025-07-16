1bf0a525ad770938345281321a2ab3bfe5769837
SerzhDares <serghdares@yandex.ru>
2025-07-16 14:56:30 +0400

    fix img path

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
   return (
     <div className="footer">
       <div className="brand_container footer_brand_container">
-        <Image src="/src/imgs/logo.png" className="brand_img footer_brand_img" />
+        <Image src="src/imgs/logo.png" className="brand_img footer_brand_img" />
         <h2 className="brand_text footer_brand_text">Best Analytics Service</h2>
       </div>
       <span className="copyright">Copyright © 2025 Analytics Dashboard</span>
